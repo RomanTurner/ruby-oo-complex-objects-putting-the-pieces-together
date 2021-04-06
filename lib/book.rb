a32acd92b7d26a32371b2a91bcbad69e3518dea3
Roman Turner <rn.turner@live.com>
2021-04-06 15:00:32 -0700

    assignment complete

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,12 @@
+class Book 
+    attr_accessor :author, :page_count, :genre # Getter and Getter
+    attr_reader :title #Getter
+    #attr_writer :example is the Setter
+    def initialize(title)
+      @title = title
+    end
 
-
+    def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end 
